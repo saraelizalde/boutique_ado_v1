14e2501c7112efe7ee641fb724b22979eab918c1
saraelizalde <j7esg480@students.codeinstitute.net>
2025-10-08 13:27:45 +0100

    Update settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -191,6 +191,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.environ.get('USE_AWS') == 'True':
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
